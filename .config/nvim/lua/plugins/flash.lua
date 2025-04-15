a68ceb4f2262dd2f7a0dd9d7cb792dad2f2e918d
Viktor Nagy <viktor@studiopresent.com>
2025-04-15 11:00:16 +0200

    Update config

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,14 +1,58 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
-  -- stylua: ignore
+  vscode = true,
+  opts = {
+    -- disable jump mode (which binds "s")
+    modes = {
+      char = {
+        enabled = true,
+        keys = { "t", "T" },
+      },
+    },
+  },
   keys = {
-    { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "F", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "s", mode = { "n", "x", "o" }, false },
+    { "S", mode = { "n", "x", "o" }, false },
+    {
+      "f",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash Jump",
+    },
+    {
+      "F",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "x", "o" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<C-s>",
+      mode = "c",
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
   },
 }
