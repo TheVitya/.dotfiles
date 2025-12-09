f4e2d2724b46cbfa33c3a81692921b2de9557444
Nagy Viktor <57563201+TheVitya@users.noreply.github.com>
2025-12-09 16:17:26 +0100

    Create conform.lua

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "biome" },
+        typescript = { "biome" },
+        javascriptreact = { "biome" },
+        typescriptreact = { "biome" },
+        json = { "biome" },
+        jsonc = { "biome" },
+        css = { "biome" },
+        ["_"] = { "trim_whitespace" },
+      },
+    },
+  },
+}
