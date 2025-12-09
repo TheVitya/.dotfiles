b99436198ae6f69b6e79553efd8171af649ea45d
Nagy Viktor <57563201+TheVitya@users.noreply.github.com>
2025-12-09 15:47:53 +0100

    Update avante.lua

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,25 +1,32 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  lazy = false,
   version = false, -- Never set this value to "*"! Never!
+  build = "make",
   opts = {
-    provider = "openai",
-    providers = {
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o-mini",
-      },
-    },
+    provider = "copilot",
+    -- provider = "openai",
+    -- providers = {
+    --   openai = {
+    --     endpoint = "https://api.openai.com/v1",
+    --     model = "gpt-4o-mini",
+    --   },
+    -- },
   },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+    -- Optional
+    "stevearc/dressing.nvim",
+    "zbirenbaum/copilot.lua", -- Required for copilot provider
+    -- Extras
     "nvim-mini/mini.pick",
     "nvim-telescope/telescope.nvim",
     "hrsh7th/nvim-cmp",
     "ibhagwan/fzf-lua",
-    "stevearc/dressing.nvim",
     "folke/snacks.nvim",
   },
+
 }
