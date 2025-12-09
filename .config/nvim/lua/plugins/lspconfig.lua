3f108899dd1f1fad3ddf5e645ce173810fb4eab4
Nagy Viktor <57563201+TheVitya@users.noreply.github.com>
2025-12-09 13:16:35 +0100

    Update lspconfig.lua

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,20 +3,21 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
       "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
     },
     opts = {
       servers = {
-        tsserver = {},
+        tsserver = {
+          keys = {
+            { "<leader>co", "<cmd>TypescriptOrganizeImports<cr>", desc = "Organize Imports" },
+            { "<leader>cR", "<cmd>TypescriptRenameFile<cr>", desc = "Rename File" },
+          },
+        },
       },
       setup = {
         tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
+          require("typescript").setup({
+            server = opts,
+          })
           return true
         end,
       },
